4e248585f196be51ddd3ba76e465f4be6464870a
Max Kang <max@puffdigital.com>
2022-01-01 17:35:23 -0500

    added struct and states

--- Origin.sol
+++ Origin.sol
@@ -7,13 +7,50 @@ This contract tracks the origin of the part and pertaining detail.
 Records producer details and allows parties involved to check the provenance of goods using a serial number or tags.
 */
 
-contract origin{
+contract origin {
 /* Variable declarataion & Modifiers/Constructors
-- Need to create a mapping/struct to identify each unique product and manufacturer
+*/
+  // Define enum 'State' with the following values:
+    enum State {
+        ProducedByFactory,   
+        ForSaleByFactory,        
+        PurchasedByDistributor, 
+        ShippedByFarmer,      
+        ReceivedByDistributor,
+        BuiltByDistributor,     
+        PackageByDistributor,   
+        ForSaleByDistributor,   
+        PurchasedByRetailer,    
+        ShippedByDistributor,    
+        ReceivedByRetailer,      
+        ForSaleByRetailer,       
+        PurchasedByConsumer     
+    }
+
+    State constant defaultState = State.ProducedByFactory;
+  
+  // Define the Item struct for the Pencil object:
+    struct Item {
+        uint     sku;                    // SKU is item ID
+        uint     upc;                    // UPC is item type, ex 2 = rubber, 3 = wood
+        address  ownerID;                // Ethereum address of the current owner as the product moves through 8 stages
+        address  originFactoryID;        // Ethereum address of the Farmer // ADDED PAYABLE
+        string   originFactoryName;      // Factory Name
+        uint     originProduceDate;      // Date item produced in factory
+        uint     productPrice;           // Product Price
+        address  distributorID;          // Ethereum address of the Distributor
+        address  retailerID;             // Ethereum address of the Retailer
+        address  consumerID;             // Ethereum address of the Consumer // ADDED payable
+        State    itemState;              // Product State as represented in the enum above
+    }
+
+/*
 - Ensure that address that is calling the contract is the admin 
     - This will become critical when connecting with multiple contracts
-
-- Make sure you specify visibility for each function 
+- TODO: EVENTS (emit on state change, item is received/produced)
+- TODO: MODIFIERS for changing product state
+- TODO: deterermine visibility and payability of functions
+- TODO: Setter/Getter methods 
 */
 
     function addManufacturer(){
