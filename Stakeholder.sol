ee05391cccbedf31dedc8a8925f65eef97f213bd
Max Kang <max@puffdigital.com>
2022-01-11 16:38:40 -0500

    added roles to stakeholder

--- Stakeholder.sol
+++ Stakeholder.sol
@@ -1,42 +1,36 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
+pragma experimental ABIEncoderV2;   
 /* 
 This contract develops a database of the parties involved
 - Works in conjunction with Track smart contract 
 - Call tracking contract at its deployed address
-
-- Recall : each line costs ETH in gas, so try to simplify
-
 */
-contract Stakeholder {
+
+import "./Origin.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract Stakeholder is AccessControl {
 /*
  Initialize mapping and struct to keep track of suppliers 
  Include constructor and modifier to assign admin and admin control
-
- The Stakeholder : owns the pencil distrbution, and can choose whether 
- or not to use the same wallet address.
-
- The Supplier    : assembles pencils (1 wood + 1 graphite + 1 eraser)
 */
+    // define contract roles
+    bytes32 public constant ADMN_ROLE = keccak256("ADMIN ROLE");
+    bytes32 public constant MNFC_ROLE = keccak256("MANUFACTURER ROLE");
+
     // define the properties of the stakeholder
     address public _owner;
     string  public _name;
-    mapping (address => supplier) public suppliers;  //  List of pencil suppliers
+    mapping (address => manufacturer) public manufacturers;  //  List of manufacturers
     mapping (address => bytes32 ) public parties;    // Stores ranks for involved parties
     
     // QMIND dev function:
     mapping (uint8 => string) public UPC_CODES;
 
-    struct supplier {
-        // Struct : Company that sells compiled pencils
-        address  _id;                               // ETH address of supplier
-        string _name;                               // name of this supplier
-        uint8 _upc;                                 // what does this supplier make?
-    }
-
     struct manufacturer {
         // Struct : Factory/Farm that produces parts
-        address _id;                                 // ETH address of manufacturer
+        address _id;                                  // ETH address of manufacturer
         string _name;                                 // name of this manufacturer
         string _location;                             // location 
         uint8 _upc;                                   // what does this manufacturer make? 
@@ -45,62 +39,78 @@ contract Stakeholder {
         // constructor keeps a list
     }
 
-    constructor(string memory name) {
+    constructor(string memory name){
         // Create a new Stakeholder 
-        _owner = (msg.sender);
+        _owner = msg.sender;
         _name = name;
     }
 
     modifier onlyOwner() {
-        require(msg.sender == _owner);
+        // make function callable only by admin
+        require(hasRole(ADMN_ROLE, msg.sender));
         _;
     }
 
+    modifier onlyMnfc() {
+        require(hasRole(MNFC_ROLE, msg.sender));
+        _;
+    }
+
+    /**
+    // we might need this extra layer of complexity at a later date...
+
     function addSupplier() public onlyOwner {
-        /*
-        Use the initial supplier parameters to input supplier credentials
-        Check that credentials are not null and are not double entries
-        */
+        //Use the initial supplier parameters to input supplier credentials
+        //Check that credentials are not null and are not double entries
     }
 
-    function removeSupplier() public onlyOwner {
+    function removeSupplier(address x) public onlyOwner {
         //Delete supplier data - admin only
+        delete manufacturers[x];
     }
 
-    function findSupplier(address s) public view returns (supplier) {
+    function findSupplier(address s) public view returns (manufacturer memory) {
         // Return supplier details based on an address input
-        // return suppliers[address]
-        return suppliers[s];
-    }
+        return manufacturers[s];
 
-    function addManufacturer(string memory name, uint256 upc) public view onlyOwner {
+    }
+    */
+
+    function addManufacturer(string memory name, string memory loc, uint8 upc) public onlyOwner {
         //Link manufacturer credentials using the mappings/strcuts created above
+        manufacturer memory x = manufacturer(msg.sender, name, loc, upc);
+        manufacturers[msg.sender] = x;
+        grantRole(MNFC_ROLE, msg.sender);
     }
 
-    function deleteManufacturer() public onlyOwner {
+    function deleteManufacturer(address x) public onlyOwner {
         //Make sure only Admin address is capable of executing this
+        revokeRole(MNFC_ROLE, x);
+        delete manufacturers[x];
     }
 
-    function findManufacturer() public onlyOwner {
+    function findManufacturer(address s) public onlyOwner view returns (manufacturer memory) {
         //This function will let any user to pull out manufacturer details using their address
-        //Essentially, finding a user in a database
+        return manufacturers[s];
     }
 
     /*
     The following functions are product specific and are similar to the manufacturer specific functions
     */
-    function addProduct() public onlyOwner {
-
+    function addProduct() public onlyMnfc {
+        // 1) construct an Origin.Item object
+        // 2) add it to the mapping
     }
 
-    function removeProduct() public onlyOwner {
-
+    function removeProduct() public onlyMnfc {
+        // 1) delete from mapping
     }
 
     function fullList() public {
     /* This is an optional feature that can be included by 
     tracking all suppliers in an array and displaying a full list when this function is called 
     */
+    // https://ethereum.stackexchange.com/questions/65589/return-a-mapping-in-a-getall-function
     }
 
     function filterGoods() public {
@@ -108,6 +118,7 @@ contract Stakeholder {
     You will need to loop through checking the goodtype to see if it matches the user entry and 
     temporarily store all the addresses to be returned
     */
+    // https://ethereum.stackexchange.com/questions/65589/return-a-mapping-in-a-getall-function
     }
 
 
